6721a55f6afd6ca29aacc488d66b9c2da278c07a
Francesco Donzello <francesco.donzello@gmail.com>
2017-11-03 15:00:45 +0100

    Update input-text.component.ts

--- src/inputs/input-text.component.ts
+++ src/inputs/input-text.component.ts
@@ -7,7 +7,7 @@ import { InlineTextConfig } from "../types/inline-configs";
     styleUrls: ["./input.component.css"],
     template: `<input #inputRef type="text" (keyup.enter)="onEnter($event)" (keyup.escape)="onEscape($event)"
                 (focus)="onFocus($event)" (blur)="onBlur($event)" (click)="onClick($event)" (keypress)="onKeyPress($event)"
-                class="form-control" [(ngModel)]="value" [required]="config.required"
+                class="input" [(ngModel)]="value" [required]="config.required"
                 [disabled]="state.isDisabled()" [name]="config.name" [placeholder]="config.placeholder"
                 [size]="config.size"/>`,
     changeDetection: ChangeDetectionStrategy.OnPush,
